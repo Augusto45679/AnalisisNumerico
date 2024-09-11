f70b92cc7f88853d09ac0f6167da1022a770cc96
Augusto Berloin <nfunes706@gmail.com>
2024-09-10 22:47:08 -0300

    Update (falta agregarle para graficar)

--- CodigosClase/EDO_CuadroGrande.m
+++ CodigosClase/EDO_CuadroGrande.m
@@ -7,7 +7,7 @@ t0=0;
 deltaT = 0.01;
 
 k1_y1 = zeros(length(t));
-tg_y1 = zeros(length(t));
+tg_y = zeros(length(t));
 yg_y1 = zeros(length(t));
 k2_y1 = zeros(length(t));
 Y_y1 =  zeros(length(t));
@@ -21,41 +21,43 @@ Y_y2 =  zeros(length(t));
 for i=1:length(t) %y1
   t_A = t(i);
 
+  %CALCULOS de K1 para y1 e y2
   k1_y1(i) = deltaT*(-10*y1+4*y2);
   k1_y1_A = k1_y1(i); %aux
 
-  tg_y1(i) = t_A + deltaT/2*w;
-  tg_y1_A = tg_y1(i); %aux
+  k1_y2(i) = deltaT*(-4*y1+0*y2);
+  k1_y2_A = k1_y2(i); %aux
 
+  %CALCULOS de TG 1 y 2
+  tg_y(i) = t_A + deltaT/2*w;
+  tg_y_A = tg_y(i); %aux
+
+  %Calculos de YG 1 y 2
   yg_y1(i) = y1 + k1_y1_A * 1/2*w;
   yg_y1_A = yg_y1(i); %aux
 
-  k2_y1(i) = deltaT * f(tg_y1_A -yg_y1_A);
+  yg_y2(i) = y1 + k1_y2_A * 1/2*w;
+  yg_y2_A = yg_y2(i); %aux
+
+  %Calculos de K2 de 1 y 2
+
+  k2_y1(i) = deltaT*(-10*yg_y1_A+4*y2);
   k2_y1_A  = k2_y1(i) ; %aux
+
+  k2_y2(i) = deltaT*(-4*yg_y2_A+0*y2);
+  k2_y2_A  = k2_y2(i) ; %aux
 
   Y_y1(i) = y1 + (1-w)*k1_y1_A + w*k2_y1_A ;
   y1 = Y_y1(i); %aux
 
-  for j=1: length(t) %y2
-
-    k1_y2(j) = deltaT*(-10*y1+4*y2);
-    k1_y2_A = k1_y2(i); %aux
-
-    tg_y2(j) = t_A + deltaT/2*w;
-    tg_y2_A = tg_y2(i); %aux
-
-    yg_y2(j) = y2 + k1_y2_A * 1/2*w;
-    yg_y2_A = yg_y1(i); %aux
-
-    k2_y2(j) = deltaT * f(tg_y1_A -yg_y1_A);
-    k2_y2_A  = k2_y2(i) ; %aux
-
-    Y_y2(j) = y2 + (1-w)*k1_y2_A + w*k2_y2_A ;
-    y2 = Y_y2(i); %aux
-
-  end
+  Y_y2(i) = y2 + (1-w)*k1_y1_A + w*k2_y1_A ;
+  y2 = Y_y2(i); %aux
 
 end
 
+for i=1:length(t)
+
+  fprintf('%.8f        %.8f       %.8f      %.8f  ',t,k1_y1(i),k2_y1(i));
+  end
 endfunction
 

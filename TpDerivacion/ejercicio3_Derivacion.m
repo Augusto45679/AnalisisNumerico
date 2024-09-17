7a630dd2bb35afe983504fb0ca40bcc4ffd8eef5
Augusto Berloin <nfunes706@gmail.com>
2024-09-17 16:26:11 -0300

    16.26

--- TpDerivacion/ejercicio3_Derivacion.m
+++ TpDerivacion/ejercicio3_Derivacion.m
@@ -1,5 +1,23 @@
-function ejercicio3_Derivacion
+pkg load symbolic;
 
+function ejercicio3_Derivacion
+pkg load symbolic;
+x0=0; xf=2;
+
+N_vector = [5,10,20];
+
+dx_vector = (xf-x0) ./ N_vector;
+
+syms t;
+y = 1/2 * (1 -exp(-2*t));
+
+%vamos a ver como hacer la deriva simbolica de esto
+dy_dt = diff(y,t);
+
+dy2_dt2 = diff(dy_dt,t);
+
+disp(dy_dt)
+disp(dy2_dt2)
 
 
  endfunction
